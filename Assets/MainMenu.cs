c662509fd821dfacb85bdd26b1db6069e64f2db3
CrazyNikkii <71390342+CrazyNikkii@users.noreply.github.com>
2023-08-23 13:29:56 +0300

    Added Crosshair

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -9,4 +9,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("SingleplayerMap");
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quitting game.");
+    }
 }
