e0b0576ce70e9e65a18d861aa653b519fc5103ae
CrazyNikkii <71390342+CrazyNikkii@users.noreply.github.com>
2023-08-23 13:41:44 +0300

    Added Volume Sliders and Music

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
     public void PlayTraining()
     {
         SceneManager.LoadScene("SingleplayerMap");
@@ -14,5 +16,18 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Quitting game.");
+    }
+
+    public void SetMasterVolume(float masterVolume)
+    {
+        audioMixer.SetFloat("masterVolume", masterVolume);
+    }
+    public void SetMusicVolume(float musicVolume)
+    {
+        audioMixer.SetFloat("musicVolume", musicVolume);
+    }
+    public void SetEffectsVolume(float effectsVolume)
+    {
+        audioMixer.SetFloat("effectsVolume", effectsVolume);
     }
 }
