6f9e929fe0613b40e3c38a74b6869adf9039a060
CrazyNikkii <71390342+CrazyNikkii@users.noreply.github.com>
2023-08-23 14:17:44 +0300

    Added PlayerPrefs save data for Volumes

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Slider masterVolumeS;
+    public Slider musicVolumeS;
+    public Slider effectsVolumeS;
+
+    public void Start()
+    {
+        if (!PlayerPrefs.HasKey("masterVolume"))
+        {
+            PlayerPrefs.SetFloat("masterVolume", 1f);
+        }
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1f);
+        }
+        if (!PlayerPrefs.HasKey("effectsVolume"))
+        {
+            PlayerPrefs.SetFloat("effectsVolume", 1f);
+        }
+        else
+        {
+            Load();
+        }
+
+        masterVolumeS.value = PlayerPrefs.GetFloat("masterVolume");
+        musicVolumeS.value = PlayerPrefs.GetFloat("musicVolume");
+        effectsVolumeS.value = PlayerPrefs.GetFloat("effectsVolume");
+    }
     public void PlayTraining()
     {
         SceneManager.LoadScene("SingleplayerMap");
@@ -16,18 +44,34 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Quitting game.");
+        Save();
     }
 
     public void SetMasterVolume(float masterVolume)
     {
         audioMixer.SetFloat("masterVolume", masterVolume);
+        PlayerPrefs.SetFloat("masterVolume", masterVolume);
     }
     public void SetMusicVolume(float musicVolume)
     {
         audioMixer.SetFloat("musicVolume", musicVolume);
+        PlayerPrefs.SetFloat("musicVolume", musicVolume);
     }
     public void SetEffectsVolume(float effectsVolume)
     {
         audioMixer.SetFloat("effectsVolume", effectsVolume);
+        PlayerPrefs.SetFloat("effectsVolume", effectsVolume);
+    }
+
+    public void Load()
+    {
+        PlayerPrefs.GetFloat("masterVolume");
+        PlayerPrefs.GetFloat("musicVolume");
+        PlayerPrefs.GetFloat("effectsVolume");
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.Save();
     }
 }
